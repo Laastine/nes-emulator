5b726c809152f478f70d91afaf8ef67de42fb01d
Mikko Kaistinen <mikko.kaistinen@kapsi.fi>
2020-01-07 08:37:17 +0200

    Rename mapper.rs variables

--- src/mapper/mod.rs
+++ src/mapper/mod.rs
@@ -18,19 +18,19 @@ impl Mapper {
   pub fn mapped_read_cpu_u8(self, address: u16) -> (bool, usize) {
     let mask = if self.prg_bank > 1 { 0x7FFF } else { 0x3FFF };
     let mapped_addr = usize::try_from(address & mask).unwrap();
-    let is_address_in_range = (0x8000..=0xFFFF).contains(&address);
-    (is_address_in_range, mapped_addr)
+    let is_mappable = (0x8000..=0xFFFF).contains(&address);
+    (is_mappable, mapped_addr)
   }
 
   pub fn mapped_read_ppu_u8(self, address: u16) -> (bool, usize) {
     let mapped_addr = usize::try_from(address).unwrap();
-    let is_address_in_range = (0x0000..=0x1FFF).contains(&address);
-    (is_address_in_range, mapped_addr)
+    let is_mappable = (0x0000..=0x1FFF).contains(&address);
+    (is_mappable, mapped_addr)
   }
 
   pub fn mapped_write_ppu_u8(self, address: u16) -> (bool, usize) {
     let mapped_addr = usize::try_from(address).unwrap();
-    let is_address_in_range = (0x0000..=0x1FFF).contains(&address);
-    (is_address_in_range, mapped_addr)
+    let is_mappable = (0x0000..=0x1FFF).contains(&address) && self.chr_bank == 0;
+    (is_mappable, mapped_addr)
   }
 }
