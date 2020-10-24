70f59d0ef0d4ee7d8a1217e216c80ccf88e64e35
Mikko Kaistinen <mikko.kaistinen@kapsi.fi>
2020-10-24 16:47:23 +0300

    Fix mock rom for tests

--- src/cartridge/mod.rs
+++ src/cartridge/mod.rs
@@ -42,20 +42,22 @@ impl Cartridge {
 
 #[cfg(test)]
 mod test {
-  use crate::cartridge::{Cartridge, CHR_ROM_BANK_SIZE, PRG_ROM_BANK_SIZE};
-  use crate::cartridge::rom_reading::{Mirroring, Rom};
-  use crate::mapper::Mapper0;
+  use crate::cartridge::Cartridge;
+  use crate::cartridge::rom_reading::Rom;
   use crate::mapper::mapper0::Mapper0;
+  use std::cell::RefCell;
+  use std::rc::Rc;
+  use crate::cartridge::rom_with_pager::RomData;
 
   impl Cartridge {
     pub fn mock_cartridge() -> Cartridge {
       let rom = Rom::mock_rom();
-      let prg_banks = rom.rom_header.prg_rom_len / PRG_ROM_BANK_SIZE;
-      let chr_banks = rom.rom_header.chr_rom_len / CHR_ROM_BANK_SIZE;
 
-      let mapper = Box::new(Mapper0::new(rom));
+      let rom_header = rom.rom_header;
+      let rom_ref = Rc::new(RefCell::new(RomData::new(rom)));
+      let mapper = Box::new(Mapper0::new(rom_ref));
 
-      Cartridge { mapper }
+      Cartridge { mapper, rom_header }
     }
   }
 }
